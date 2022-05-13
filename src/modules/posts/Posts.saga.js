eaf1f94463e3531522f1a71ed176bd3fb778ae93
Marc Cavazos <marcavazos19@icloud.com>
2022-05-12 22:05:09 -0500

    tutorial

---
+++ src/modules/posts/Posts.saga.js
@@ -0,0 +1,37 @@
+import { takeEvery, all, call, put, takeLeading } from 'redux-saga/effects';
+import { GET_POSTS, getPostsSuccess, getPostsFailed, ADD_POST,
+addPostFailed, addPostSuccess } from './Posts.actions';
+import { getPosts, addPost } from '../../utils/api';
+
+function* getPostsSaga() {
+    try {
+        const data = yield call(getPosts);
+        yield put(getPostsSuccess(data));
+    } catch(e) {
+        yield put(getPostsFailed(e.message));
+    }
+}
+
+function* getPostsWatcher() {
+    yield takeEvery(GET_POSTS, getPostsSaga)
+}
+
+function* addPostSaga(action) {
+    try {
+        const data = yield call(addPost, action.payload);
+        yield put(addPostSuccess({...action.payload, ...data}));
+    } catch(e) {
+        yield put(addPostFailed(e.message));
+    }
+}
+
+function* addPostWatcher() {
+    yield takeLeading(ADD_POST, addPostSaga)
+}
+
+export default function* postsSaga() {
+    yield all([
+        getPostsWatcher(),
+        addPostWatcher()
+    ])
+}
